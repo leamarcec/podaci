e4c0ab6d0775202c9668444da0fc9d2078ff7b08
leamarcec <45050812+leamarcec@users.noreply.github.com>
2019-01-06 22:42:32 +0100

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,239 @@
+﻿namespace podaci
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.unos = new System.Windows.Forms.Label();
+            this.ime = new System.Windows.Forms.Label();
+            this.prezime = new System.Windows.Forms.Label();
+            this.adresa = new System.Windows.Forms.Label();
+            this.posta = new System.Windows.Forms.Label();
+            this.imeTextBox = new System.Windows.Forms.TextBox();
+            this.prezimeTextBox = new System.Windows.Forms.TextBox();
+            this.adresaTextBox = new System.Windows.Forms.TextBox();
+            this.postaTextBox = new System.Windows.Forms.TextBox();
+            this.drzava = new System.Windows.Forms.Label();
+            this.datumrodenja = new System.Windows.Forms.Label();
+            this.oib = new System.Windows.Forms.Label();
+            this.drzavaTextBox = new System.Windows.Forms.TextBox();
+            this.datumrodenjaTextBox = new System.Windows.Forms.TextBox();
+            this.oibTextBox = new System.Windows.Forms.TextBox();
+            this.ispisButton = new System.Windows.Forms.Button();
+            this.ispisTextBox = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // unos
+            // 
+            this.unos.AutoSize = true;
+            this.unos.Location = new System.Drawing.Point(40, 17);
+            this.unos.Name = "unos";
+            this.unos.Size = new System.Drawing.Size(131, 17);
+            this.unos.TabIndex = 0;
+            this.unos.Text = "UNOS PODATAKA:";
+            // 
+            // ime
+            // 
+            this.ime.AutoSize = true;
+            this.ime.Location = new System.Drawing.Point(40, 50);
+            this.ime.Name = "ime";
+            this.ime.Size = new System.Drawing.Size(34, 17);
+            this.ime.TabIndex = 1;
+            this.ime.Text = "Ime:";
+            // 
+            // prezime
+            // 
+            this.prezime.AutoSize = true;
+            this.prezime.Location = new System.Drawing.Point(40, 85);
+            this.prezime.Name = "prezime";
+            this.prezime.Size = new System.Drawing.Size(63, 17);
+            this.prezime.TabIndex = 2;
+            this.prezime.Text = "Prezime:";
+            // 
+            // adresa
+            // 
+            this.adresa.AutoSize = true;
+            this.adresa.Location = new System.Drawing.Point(40, 122);
+            this.adresa.Name = "adresa";
+            this.adresa.Size = new System.Drawing.Size(57, 17);
+            this.adresa.TabIndex = 3;
+            this.adresa.Text = "Adresa:";
+            // 
+            // posta
+            // 
+            this.posta.AutoSize = true;
+            this.posta.Location = new System.Drawing.Point(40, 159);
+            this.posta.Name = "posta";
+            this.posta.Size = new System.Drawing.Size(76, 17);
+            this.posta.TabIndex = 4;
+            this.posta.Text = "Broj poste:";
+            // 
+            // imeTextBox
+            // 
+            this.imeTextBox.Location = new System.Drawing.Point(172, 45);
+            this.imeTextBox.Name = "imeTextBox";
+            this.imeTextBox.Size = new System.Drawing.Size(122, 22);
+            this.imeTextBox.TabIndex = 5;
+            // 
+            // prezimeTextBox
+            // 
+            this.prezimeTextBox.Location = new System.Drawing.Point(172, 80);
+            this.prezimeTextBox.Name = "prezimeTextBox";
+            this.prezimeTextBox.Size = new System.Drawing.Size(122, 22);
+            this.prezimeTextBox.TabIndex = 6;
+            // 
+            // adresaTextBox
+            // 
+            this.adresaTextBox.Location = new System.Drawing.Point(172, 117);
+            this.adresaTextBox.Name = "adresaTextBox";
+            this.adresaTextBox.Size = new System.Drawing.Size(248, 22);
+            this.adresaTextBox.TabIndex = 7;
+            // 
+            // postaTextBox
+            // 
+            this.postaTextBox.Location = new System.Drawing.Point(172, 154);
+            this.postaTextBox.Name = "postaTextBox";
+            this.postaTextBox.Size = new System.Drawing.Size(122, 22);
+            this.postaTextBox.TabIndex = 8;
+            // 
+            // drzava
+            // 
+            this.drzava.AutoSize = true;
+            this.drzava.Location = new System.Drawing.Point(509, 50);
+            this.drzava.Name = "drzava";
+            this.drzava.Size = new System.Drawing.Size(57, 17);
+            this.drzava.TabIndex = 9;
+            this.drzava.Text = "Drzava:";
+            // 
+            // datumrodenja
+            // 
+            this.datumrodenja.AutoSize = true;
+            this.datumrodenja.Location = new System.Drawing.Point(509, 85);
+            this.datumrodenja.Name = "datumrodenja";
+            this.datumrodenja.Size = new System.Drawing.Size(105, 17);
+            this.datumrodenja.TabIndex = 10;
+            this.datumrodenja.Text = "Datum rodenja:";
+            // 
+            // oib
+            // 
+            this.oib.AutoSize = true;
+            this.oib.Location = new System.Drawing.Point(509, 122);
+            this.oib.Name = "oib";
+            this.oib.Size = new System.Drawing.Size(35, 17);
+            this.oib.TabIndex = 11;
+            this.oib.Text = "OIB:";
+            // 
+            // drzavaTextBox
+            // 
+            this.drzavaTextBox.Location = new System.Drawing.Point(622, 50);
+            this.drzavaTextBox.Name = "drzavaTextBox";
+            this.drzavaTextBox.Size = new System.Drawing.Size(111, 22);
+            this.drzavaTextBox.TabIndex = 12;
+            // 
+            // datumrodenjaTextBox
+            // 
+            this.datumrodenjaTextBox.Location = new System.Drawing.Point(622, 85);
+            this.datumrodenjaTextBox.Name = "datumrodenjaTextBox";
+            this.datumrodenjaTextBox.Size = new System.Drawing.Size(111, 22);
+            this.datumrodenjaTextBox.TabIndex = 13;
+            // 
+            // oibTextBox
+            // 
+            this.oibTextBox.Location = new System.Drawing.Point(622, 122);
+            this.oibTextBox.Name = "oibTextBox";
+            this.oibTextBox.Size = new System.Drawing.Size(137, 22);
+            this.oibTextBox.TabIndex = 14;
+            // 
+            // ispisButton
+            // 
+            this.ispisButton.Location = new System.Drawing.Point(43, 212);
+            this.ispisButton.Name = "ispisButton";
+            this.ispisButton.Size = new System.Drawing.Size(121, 37);
+            this.ispisButton.TabIndex = 15;
+            this.ispisButton.Text = "Ispis podataka";
+            this.ispisButton.UseVisualStyleBackColor = true;
+            this.ispisButton.Click += new System.EventHandler(this.ispisButton_Click);
+            // 
+            // ispisTextBox
+            // 
+            this.ispisTextBox.Location = new System.Drawing.Point(43, 271);
+            this.ispisTextBox.Multiline = true;
+            this.ispisTextBox.Name = "ispisTextBox";
+            this.ispisTextBox.Size = new System.Drawing.Size(286, 167);
+            this.ispisTextBox.TabIndex = 16;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.ispisTextBox);
+            this.Controls.Add(this.ispisButton);
+            this.Controls.Add(this.oibTextBox);
+            this.Controls.Add(this.datumrodenjaTextBox);
+            this.Controls.Add(this.drzavaTextBox);
+            this.Controls.Add(this.oib);
+            this.Controls.Add(this.datumrodenja);
+            this.Controls.Add(this.drzava);
+            this.Controls.Add(this.postaTextBox);
+            this.Controls.Add(this.adresaTextBox);
+            this.Controls.Add(this.prezimeTextBox);
+            this.Controls.Add(this.imeTextBox);
+            this.Controls.Add(this.posta);
+            this.Controls.Add(this.adresa);
+            this.Controls.Add(this.prezime);
+            this.Controls.Add(this.ime);
+            this.Controls.Add(this.unos);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label unos;
+        private System.Windows.Forms.Label ime;
+        private System.Windows.Forms.Label prezime;
+        private System.Windows.Forms.Label adresa;
+        private System.Windows.Forms.Label posta;
+        private System.Windows.Forms.TextBox imeTextBox;
+        private System.Windows.Forms.TextBox prezimeTextBox;
+        private System.Windows.Forms.TextBox adresaTextBox;
+        private System.Windows.Forms.TextBox postaTextBox;
+        private System.Windows.Forms.Label drzava;
+        private System.Windows.Forms.Label datumrodenja;
+        private System.Windows.Forms.Label oib;
+        private System.Windows.Forms.TextBox drzavaTextBox;
+        private System.Windows.Forms.TextBox datumrodenjaTextBox;
+        private System.Windows.Forms.TextBox oibTextBox;
+        private System.Windows.Forms.Button ispisButton;
+        private System.Windows.Forms.TextBox ispisTextBox;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace podaci
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void ispisButton_Click(object sender, EventArgs e)
+        {
+            string ime = imeTextBox.Text;
+            string prezime = prezimeTextBox.Text;
+            string adresa = adresaTextBox.Text;
+            string posta = postaTextBox.Text;
+            string drzava = drzavaTextBox.Text;
+            string datumrodenja = datumrodenjaTextBox.Text;
+            string oib = oibTextBox.Text;
+
+            string ispis = ime + prezime + adresa + posta + drzava + datumrodenja + oib;
+            ispisTextBox.Text = ispis;
+
+
+        }
+    }
+}
